0663d16a67c1d03d02909281c467c2e2d6de3aad
Gotmachine <24925209+gotmachine@users.noreply.github.com>
2020-08-27 00:14:46 +0200

    Flic flac

--- CornetEMVisu/Resources.Designer.cs
+++ CornetEMVisu/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace CornetEMVisu {
+namespace Remous {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace CornetEMVisu {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CornetEMVisu.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Remous.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
